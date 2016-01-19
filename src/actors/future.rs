cc8ceeb28fd269db89a09a0d86cc2589fbcdedd0
Felix Raimundo <gamaz3ps@gmail.com>
2016-01-19 22:00:56 +0100

    WIP - Futures as actors.
    Also improve the imports in name_resolver.

--- src/actors/future.rs
+++ src/actors/future.rs
@@ -1,73 +1,72 @@
 use std::any::Any;
-use std::collections::VecDeque;
 use std::sync::Mutex;
 
-pub struct Future {
-    /// Value contained inside the future.
-    value: Mutex<Option<Box<Any>>>,
-    /// Closures to cal on the value.
-    closures: Mutex<VecDeque<Box<Fn(Box<Any>) -> Box<Any>>>>,
-    /// State of the future.
-    state: Mutex<FutureState>,
+use actors::{Actor, ActorCell, ActorContext, ActorPath, ActorRef};
+
+enum FutureMessages {
+    Complete(Box<Any>),
+    Calculation(Box<Any>),
+    Extract,
 }
 
 #[derive(PartialEq)]
 enum FutureState {
-    /// Future is created, but the value is not yet filled.
-    New,
-    /// The value has been completed, but computation have not been started yet.
-    Completed,
-    /// Computations have been run.
-    /// NOTE: this does not mean that a computation is running right now, thus that is not super
-    /// Sync.
+    Uncompleted,
     Computing,
-    /// The value from the Future has been extracted, nothing can be done with it now.
-    Taken,
+    Terminated,
+    Extracted,
+}
+
+struct Future {
+    value: Mutex<Option<Box<Any + Send>>>,
+    state: Mutex<FutureState>,
 }
 
 impl Future {
-    pub fn new() -> Future {
+    fn new() -> Future {
         Future {
             value: Mutex::new(None),
-            closures: Mutex::new(VecDeque::new()),
-            state: Mutex::new(FutureState::New),
+            state: Mutex::new(FutureState::Uncompleted),
         }
     }
+}
 
-    pub fn complete(&self, value: Box<Any>) {
-        let mut state = self.state.lock().unwrap();
-        if *state == FutureState::New {
-            *self.value.lock().unwrap() = Some(value);
-            *state = FutureState::Completed;
-        } else {
-            panic!("Tried to complete a future in a bad state");
-        }
-    }
+impl Actor for Future {
+    fn receive(&self, message: Box<Any>, context: ActorCell) {
+        // NOTE: We may want to fail if the message is not correct.
+        if let Ok(message) = Box::<Any>::downcast::<FutureMessages>(message) {
+            match *message {
+                FutureMessages::Complete(msg) => {
+                    let mut state = self.state.lock().unwrap();
+                    if *state != FutureState::Uncompleted {
+                        // NOTE: Send a failure to the sender instead.
+                        panic!("Tried to complete a Future twice");
+                    }
+                    *self.value.lock().unwrap() = Some(msg);
+                    *state = FutureState::Computing;
+                },
+                FutureMessages::Calculation(msg) => {
+                    // FIXME(gamazeps): check the state.
+                    if let Ok(func) = Box::<Any>::downcast::<Fn(Box<Any>, ActorCell) -> Option<Box<Any>>>(message) {
+                        let mut value = self.value.lock().unwrap();
+                        let v = value.take().unwrap();
+                        *value = (*func)(v, context);
+                        if *value == None {
+                            *self.state.lock().unwrap() = FutureState::Terminated;
+                            context.kill_me();
+                        }
+                    }
 
-    pub fn handle(&self) {
-        // FIXME(gamazeps): check that the state is clean in a better way.
-        if *self.state.lock().unwrap() == FutureState::Completed ||
-            *self.state.lock().unwrap() == FutureState::Computing {
-                let func = self.closures.lock().unwrap().pop_front();
-                if let Some(func) = func {
+                },
+                FutureMessages::Extract => {
                     let mut value = self.value.lock().unwrap();
-                    *value = Some(func(value.take().unwrap()));
-                }
-                *self.state.lock().unwrap() = FutureState::Computing;
+                    let v = value.take();
+                    *value = None;
+                    *self.state.lock().unwrap() = FutureState::Extracted;
+                    context.tell(context.sender(), v);
+                    context.kill_me()
+                },
             }
-    }
-
-    pub fn extract(&self) -> Option<Box<Any>> {
-        let mut value = self.value.lock().unwrap();
-        let v = value.take();
-        *value = None;
-        *self.state.lock().unwrap() = FutureState::Taken;
-        v
-    }
-
-    // NOTE: I'm super no sure about the 'static lifetime.
-    pub fn then_do<F: Fn(Box<Any>) -> Box<Any> + 'static>(&self, closure: F) {
-        self.closures.lock().unwrap().push_back(Box::new(closure));
-        // FIXME(gamazeps): schedule the actor.
+        }
     }
 }
